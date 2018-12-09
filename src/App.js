612eabe700acffb4981e130fcd42416cd38650a6
Tenzin Migmar <lhasaboy2010@gmail.com>
2018-12-09 14:03:21 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.scss'
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 
 import AuthenticatedRoute from './auth/components/AuthenticatedRoute'
